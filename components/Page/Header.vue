802ba3c61cc980f39e5d3f042ec63db1bc490aff
Anatoly <veshaginanatoly@gmail.com>
2024-01-23 00:29:09 +0300

    Проверка поиска по заголовку товара

--- components/Page/Header.vue
+++ components/Page/Header.vue
@@ -1,18 +1,20 @@
 <template>
-  <nav class="mx-auto bg-green-100">
-    <ul>
-      <div class="flex flex-wrap items-center gap-x-2 p-4">
-        <li
-          class="py-2 px-4 rounded-lg border hover:bg-green-300 hover:ring-2 hover:ring-green-500"
-        >
-          <NuxtLink to="/products">Все продукты</NuxtLink>
-        </li>
-        <li
-          class="py-2 px-4 rounded-lg border hover:bg-green-300 hover:ring-2 hover:ring-green-500"
-        >
-          <NuxtLink to="/products/create">Добавить продукт</NuxtLink>
-        </li>
-      </div>
-    </ul>
-  </nav>
+  <header>
+    <nav class="mx-auto bg-green-100">
+      <ul>
+        <div class="flex flex-wrap items-center gap-x-2 p-4">
+          <li
+            class="py-2 px-4 rounded-lg border hover:bg-green-300 hover:ring-2 hover:ring-green-500"
+          >
+            <NuxtLink to="/products">Все продукты</NuxtLink>
+          </li>
+          <li
+            class="py-2 px-4 rounded-lg border hover:bg-green-300 hover:ring-2 hover:ring-green-500"
+          >
+            <NuxtLink to="/products/create">Добавить продукт</NuxtLink>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  </header>
 </template>
--- components/Products/List.vue
+++ components/Products/List.vue
@@ -1,30 +1,60 @@
 <template>
   <PageHeader />
-  <main class="max-w-screen-lg mx-auto flex flex-col">
-    <!-- product card -->
-    <div
-      v-for="product in products"
-      class="flex flex-col md:flex-row overflow-hidden rounded-lg border shadow-xl mt-4 w-100 mx-2"
-    >
-      <!-- thumbnail -->
-      <div class="h-64 md:h-32 w-auto md:w-1/2">
-        <a :href="`/products/edit/${product._id}`">
-          <img
-            :src="product.thumbnail"
-            class="inset-0 h-full w-full object-cover object-center"
-          />
-        </a>
+  <main class="max-w-screen-lg mx-auto">
+    <div class="grid grid-cols-[240px_1fr]">
+      <div class="flex flex-col">
+        <!-- siderbar -->
+        <div class="shadow-xl border mt-4 rounded-lg py-2 px-2">
+          <label class="relative block">
+            <button class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg
+                class="h-6 w-6 fill-slate-300 hover:fill-slate-600"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <input
+              class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-lg py-2 pl-9 pr-3 shadow-lg focus:outline-none sm:text-sm"
+              type="text"
+              name="search"
+              v-model="search"
+              placeholder="Поиск..."
+            />
+          </label>
+        </div>
       </div>
-      <!-- content -->
-      <div class="w-full py-4 px-6 flex flex-col justify-between">
-        <h3 class="font-semibold text-lg leading-tight truncate">
-          {{ product.title }}
-        </h3>
-        <p>Бренд товара: {{ product.brand }}</p>
-        <p>Рейтинг товара: {{ product.rating }}</p>
-      </div>
-      <div class="w-64 py-4 px-6 flex flex-row items-center justify-center">
-        <h2 class="font-bold text-lg">{{ product.price }} ₽</h2>
+      <div class="flex flex-col">
+        <!-- product card -->
+        <div
+          v-for="product in filteredProducts"
+          class="flex flex-col md:flex-row rounded-lg border shadow-xl mt-4 mx-2"
+        >
+          <!-- thumbnail -->
+          <div class="h-64 md:h-32 w-auto md:w-1/2">
+            <a :href="`/products/edit/${product._id}`">
+              <img
+                :src="product.thumbnail"
+                class="inset-0 h-full w-full object-cover object-center"
+              />
+            </a>
+          </div>
+          <!-- content -->
+          <div class="w-full py-4 px-6 flex flex-col justify-between">
+            <h3 class="font-semibold text-lg leading-tight truncate">
+              {{ product.title }}
+            </h3>
+            <p>Бренд товара: {{ product.brand }}</p>
+            <p>Рейтинг товара: {{ product.rating }}</p>
+          </div>
+          <div class="w-64 py-4 px-6 flex flex-row items-center justify-center">
+            <h2 class="font-bold text-lg">{{ product.price }} ₽</h2>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -32,12 +62,19 @@
 
 <script setup>
 const products = ref([]);
+const search = ref("");
 
 async function getProducts() {
   const fetchedProducts = await $fetch("/api/products");
   console.log(fetchedProducts);
   products.value = fetchedProducts || [];
 }
+
+const filteredProducts = computed(() => {
+  return products.value.filter((product) => {
+    return product.title.toLowerCase().includes(search.value.toLowerCase());
+  });
+});
 
 onMounted(getProducts);
 </script>
