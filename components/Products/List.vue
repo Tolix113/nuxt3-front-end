4ca6bb9027140a0f13af43119321c1636907d63e
Anatoly <veshaginanatoly@gmail.com>
2024-01-23 20:32:29 +0300

    Правка верстки. Добавлена пара новых фильтров

--- components/Products/List.vue
+++ components/Products/List.vue
@@ -1,10 +1,10 @@
 <template>
   <PageHeader />
   <main class="max-w-screen-lg mx-auto">
-    <div class="grid grid-cols-[240px_1fr]">
-      <div class="flex flex-col">
+    <div class="flex flex-row flex-wrap w-full">
+      <div class="flex flex-col w-full mx-2 md:w-[240px]">
         <!-- siderbar -->
-        <div class="shadow-xl border mt-4 rounded-lg py-2 px-2">
+        <div class="border mt-4 rounded-lg py-2 px-2">
           <label class="relative block">
             <button class="absolute inset-y-0 left-0 flex items-center pl-2">
               <svg
@@ -19,34 +19,62 @@
               </svg>
             </button>
             <input
-              class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-lg py-2 pl-9 pr-3 shadow-lg focus:outline-none sm:text-sm"
+              class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-lg py-2 pl-9 pr-3 shadow-md focus:outline-none sm:text-sm"
               type="text"
               name="search"
               v-model="search"
               placeholder="Поиск..."
             />
           </label>
+          <label
+            class="block mt-2 pl-1 font-semibold"
+            for="fromPrice"
+            >Цена:</label
+          >
           <input
-            class="mt-2 placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-lg py-2 pl-5 pr-3 shadow-lg focus:outline-none sm:text-sm"
+            class="block mt-2 placeholder:text-slate-400 bg-white w-full border border-slate-300 rounded-lg py-2 pl-5 pr-3 shadow-md focus:outline-none sm:text-sm"
             id="fromPrice"
             type="number"
-            v-model="fromPrice"
-            placeholder="От 0"
+            v-model.number="fromPrice"
+            :placeholder="`От ${minPrice}`"
           />
           <input
-            class="mt-2 placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-lg py-2 pl-5 pr-3 shadow-lg focus:outline-none sm:text-sm"
+            class="block mt-2 placeholder:text-slate-400 bg-white w-full border border-slate-300 rounded-lg py-2 pl-5 pr-3 shadow-md focus:outline-none sm:text-sm"
             id="toPrice"
             type="number"
-            v-model="toPrice"
-            placeholder="До 999999"
+            v-model.number="toPrice"
+            :placeholder="`До ${maxPrice}`"
           />
+          <label class="flex items-center mt-2">
+            <input
+              class="mr-1"
+              type="checkbox"
+              v-model="inStock"
+            />
+            В наличии
+          </label>
+          <span class="block font-semibold">Бренды:</span>
+          <div
+            class="flex flex-col"
+            v-for="brand in brands"
+          >
+            <label>
+              <input
+                id="brands"
+                type="checkbox"
+                :value="brand"
+                v-model="selectedBrands"
+              />
+              {{ brand }}
+            </label>
+          </div>
         </div>
       </div>
-      <div class="flex flex-col">
+      <div class="flex flex-col w-full md:w-[calc(100%-256px)]">
         <!-- product card -->
         <div
           v-for="product in filteredProducts"
-          class="flex flex-col md:flex-row rounded-lg border shadow-xl mt-4 mx-2"
+          class="flex flex-col md:flex-row rounded-lg border shadow-md mt-4 mx-2"
         >
           <!-- thumbnail -->
           <div class="h-64 md:h-32 w-auto md:w-1/2">
@@ -65,7 +93,9 @@
             <p>Бренд товара: {{ product.brand }}</p>
             <p>Рейтинг товара: {{ product.rating }}</p>
           </div>
-          <div class="w-64 py-4 px-6 flex flex-row items-center justify-center">
+          <div
+            class="w-64 py-4 px-6 flex flex-row items-center justify-start md:justify-center"
+          >
             <h2 class="font-bold text-lg">{{ product.price }} ₽</h2>
           </div>
         </div>
@@ -79,27 +109,76 @@ const products = ref([]);
 const search = ref("");
 const fromPrice = ref("");
 const toPrice = ref("");
+const inStock = ref(false);
+const selectedBrands = ref([]);
+let maxPrice = 0;
+let minPrice = 0;
+
+const categories = ["smartphones"];
+const brands = ["Apple", "Samsung"];
 
 async function getProducts() {
   const fetchedProducts = await $fetch("/api/products");
   products.value = fetchedProducts.items || [];
-  fromPrice.value = fetchedProducts.minPrice;
-  toPrice.value = fetchedProducts.maxPrice;
+  maxPrice = fetchedProducts.maxPrice;
+  minPrice = fetchedProducts.minPrice;
+  fromPrice.value = minPrice;
+  toPrice.value = maxPrice;
 }
 
 const filteredProducts = computed(() => {
-  return products.value.filter(filteredBySearch).filter(fileteredByPrice);
+  let p = products.value;
+
+  if (search.value) {
+    p = p.filter(filterBySearch);
+  }
+
+  if (fromPrice.value || toPrice.value) {
+    p = p.filter(filterByPrice);
+  }
+
+  if (inStock.value) {
+    p = p.filter(filterByStock);
+  }
+
+  if (selectedBrands.value.length > 0) {
+    p = p.filter(filterByBrand);
+  }
+
+  return p;
 });
 
-const filteredBySearch = (item) => {
+const filterBySearch = (item) => {
   return item.title.toLowerCase().includes(search.value.toLowerCase());
 };
 
-const fileteredByPrice = (product) => {
-  return product.price >= fromPrice.value && product.price <= toPrice.value;
+const filterByPrice = (product) => {
+  const filterFromPrice = fromPrice.value ? fromPrice.value : minPrice;
+  const filterToPrice = toPrice.value ? toPrice.value : maxPrice;
+  return product.price >= filterFromPrice && product.price <= filterToPrice;
 };
 
-console.log(filteredProducts.value);
+const filterByStock = (product) => {
+  return product.stock > 0;
+};
+
+const filterByBrand = (product) => {
+  return selectedBrands.value.includes(product.brand);
+};
+
+watch([fromPrice, toPrice], () => {
+  if (fromPrice.value <= 0) {
+    fromPrice.value = "";
+  }
+
+  if (toPrice.value <= 0) {
+    toPrice.value = "";
+  }
+});
+
+watch(selectedBrands, () => {
+  console.log(selectedBrands);
+});
 
 onMounted(getProducts);
 </script>
