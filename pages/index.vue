bdae283f95e28df0ab95bbb5974e1b78c463d9af
Anatoly <veshaginanatoly@gmail.com>
2024-01-30 02:47:05 +0300

    Обновление версии nuxt и vue. Добавление библиотеки для пагинации

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
   <PageHeader />
+  <vue-awesome-paginate
+    :total-items="50"
+    v-model="currentPage"
+  >
+  </vue-awesome-paginate>
 </template>
+
+<script setup>
+const currentPage = ref(1);
+</script>
