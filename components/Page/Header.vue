d0ff2d11fe4aa10756c470ddf9a60194094cc7c5
Anatoly <veshaginanatoly@gmail.com>
2024-02-04 17:14:40 +0300

    Заглушки для добавленных товаров. Fix фронтенда

--- components/Page/Header.vue
+++ components/Page/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="mx-auto bg-green-100">
       <ul>
-        <div class="flex flex-wrap items-center gap-x-2 p-4">
+        <div class="flex flex-wrap justify-center items-center gap-x-2 p-4">
           <li
             class="py-2 px-4 rounded-lg border hover:bg-green-300 hover:ring-2 hover:ring-green-500"
           >
